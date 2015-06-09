5d8f32f754cf9d7b6a119ddc9667450b640a9a17
jianjie.wu <jianjie.wu@renren-inc.com>
2015-06-09 18:06:03 +0800

    custom pem

--- lib/alipay/sign/rsa.rb
+++ lib/alipay/sign/rsa.rb
@@ -5,12 +5,15 @@ module Alipay
   module Sign
     class RSA
       def self.sign(key, string)
+      binding.pry
         rsa = OpenSSL::PKey::RSA.new(key)
         Base64.encode64(rsa.sign('sha1', string))
       end
 
       def self.verify?(key, string, sign)
-        rsa = OpenSSL::PKey::RSA.new(key)
+binding.pry
+
+rsa = OpenSSL::PKey::RSA.new(key)
         rsa.verify('sha1', Base64.decode64(sign), string)
       end
     end
